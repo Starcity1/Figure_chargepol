dc4e6f4a653b189cd863421418b89e7c9e2999c0
kJo75 <96453839+kJo75@users.noreply.github.com>
2023-05-19 02:32:26 -0500

    Update prepare_data.py

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -7,9 +7,10 @@
 # Dependencies : seaborn, numpy, matplotlib, Python 3.6+
 # Usage :: python3 prepare_data.py <path_to_charepol_csv_file>
 ##
-
+import os
 from os import path
 import csv
+
 
 def verify_data(filepath) -> bool:
     with open(filepath, 'r') as chargepol_file:
@@ -18,7 +19,6 @@ def verify_data(filepath) -> bool:
         if "longitude" not in header:
             return False
     return True
-
 
 
 def get_data(filepath):
@@ -45,4 +45,46 @@ def get_data(filepath):
                   "Charge": chargeEvent, # Type of charge, length and starting altitude
                   "Location": longLat    # Longitude and altitude of charge
                 }
+    return chargepol
+
+
+def get_multipleData(directory):
+    files = os.listdir(directory)
+
+    iterations = 0 #This variable will be used to store how many days we have iterated through
+    time = list()
+    chargeEvent = list()
+    longLat = list()
+    date = list()
+
+    chargepol = {"Timestamp": time,  # Time of event
+                 "Charge": chargeEvent,  # Type of charge, length and starting altitude
+                 "Location": longLat,  # Longitude and altitude of charge
+                 "Date": date  # MM/DD. ex. 05/05
+                 }
+
+    for file in files:
+        file_path = os.path.join(directory, file)  # Create the full file path
+        """Creates a custom data frame of the charge pol data."""
+        if not (path.exists(file_path) and file_path[-4:] == '.csv'):
+            exit("Error: file must be a .csv file.")
+
+        if not verify_data(file_path):
+            exit("Error: please used the forked version of chargepol")
+
+        month = file[-8:-6]
+        day = file[-6:-4]
+        monthAndDay = month + "/" + day
+        date.append(monthAndDay)
+
+        with open(file_path, 'r', newline='') as csv_file:
+            reader = csv.reader(csv_file)
+            for row in reader:
+                if(row[0] != "pos") and (row[0] != "neg") : continue
+                time.append((float(row[1]))+(iterations*86400))
+                chargeEvent.append([row[0], float(row[2]), float(row[3])])
+                longLat.append([row[-1], row[-2]])
+        iterations += 1
+
+
     return chargepol
